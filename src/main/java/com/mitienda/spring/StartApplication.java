d8abfb1cc385de8f6aca6b9bc6348c6e8f483321
Castelao <diegochenlo1@gmail.com>
2019-11-26 12:51:05 +0100

    --PRO2 Spring

--- src/main/java/com/mitienda/spring/StartApplication.java
+++ src/main/java/com/mitienda/spring/StartApplication.java
@@ -1,17 +1,21 @@
 package com.mitienda.spring;
 
 import org.slf4j.Logger;
+
+
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import src.menus.MenuController;
+
 @SpringBootApplication
 public class StartApplication implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(StartApplication.class);
-
+    
     @Autowired
     private CategoriasRepository repository;
 
@@ -21,7 +25,7 @@ public class StartApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-
+    	/*
         log.info("StartApplication...");
 
         repository.save(new Categorias("Java"));
@@ -36,7 +40,13 @@ public class StartApplication implements CommandLineRunner {
 
         System.out.println("\nfindByName('Node')");
         repository.findByName("Node").forEach(x -> System.out.println(x));
+        */
+        
+    	MenuController.getInstance().callMenus();
 
     }
+	
+    
+	
 
 }
